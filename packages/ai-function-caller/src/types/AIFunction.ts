a1f0c16a214d81ab7a5ad729de8db2c4455ce00c
Ralph Schuler <ralph.schuler@nyphon.de>
2023-11-26 22:53:11 +0100

    docs(ai-function-caller): add ts doc comments

--- packages/ai-function-caller/src/types/AIFunction.ts
+++ packages/ai-function-caller/src/types/AIFunction.ts
@@ -1,31 +1,95 @@
+/**
+ * Represents the name of an AI function.
+ */
 export type AIFunctionName = string;
+
+/**
+ * Describes an AI function.
+ */
 export type AIFunctionDescription = string;
+
+/**
+ * Represents the name of a parameter in an AI function.
+ */
 export type AIFunctionParameterName = string;
+
+/**
+ * Enumerates the possible types for AI function properties.
+ */
 export type AIPropertyType = "string" | "integer" | "boolean" | "object";
+
+/**
+ * Represents the name of a property in an AI function.
+ */
 export type AIFunctionPropertyName = string;
+
+/**
+ * Describes a property in an AI function.
+ */
 export type AIPropertyDescription = string;
+
+/**
+ * Represents a property of an AI function, including its type and description.
+ */
 export type AIFunctionProperty = {
+  /** The type of the property. */
   type: AIPropertyType;
+  /** The description of the property. */
   description: AIPropertyDescription;
 };
+
+/**
+ * Represents the parameters of an AI function, including their types and required status.
+ */
 export type AIFunctionParameters = {
+  /** The type of the parameters, typically an object. */
   type: "object";
+  /** The properties of the parameters, keyed by the parameter name. */
   properties: {
     [key: AIFunctionParameterName]: AIFunctionProperty;
   };
+  /** An array of parameter names that are required. */
   required: AIFunctionParameterName[];
 };
+
+/**
+ * Represents a parameter value in an AI function call, which can be a string, number, or boolean.
+ */
 export type AIFunctionParameter = string | number | boolean;
+
+/**
+ * Represents a call to an AI function, including the function name and arguments.
+ */
 export type AIFunctionCall = {
+  /** The name of the AI function being called. */
   name: AIFunctionName;
+  /** The arguments passed to the AI function. */
   arguments: AIFunctionCallArguments;
 };
+
+/**
+ * Represents the arguments passed in an AI function call.
+ */
 export type AIFunctionCallArguments = string;
+
+/**
+ * Represents an example usage of an AI function.
+ */
 export type AIFunctionExample = string;
+
+/**
+ * Represents an AI function, including its name, description, parameters, and the method to execute it.
+ * @template AiFunctionCallResponse The expected response type from the AI function call.
+ */
 export type AIFunction<AiFunctionCallResponse> = {
+  /** The name of the AI function. */
   name: AIFunctionName;
+  /** A description of the AI function. */
   description: AIFunctionDescription;
+  /** The parameters required by the AI function. */
   parameters: AIFunctionParameters;
+  /** The method to execute the AI function. */
   method: (args: AIFunctionCallArguments) => Promise<AiFunctionCallResponse>;
+  /** An optional example of how the AI function can be used. */
   example?: AIFunctionExample;
 };
--- packages/state-store/src/types/Interceptors.type.ts
+++ packages/state-store/src/types/Interceptors.type.ts
@@ -1,7 +1,13 @@
 import { Interceptor } from "./Interceptor.type.js";
 
 /**
- * Type definition for interceptors. Keyed by state property names.
+ * This type defines a structure for storing multiple interceptors based on state property names.
+ * The `Interceptors` type is an object where each key is a property name from the `StateType` and the value is an optional array of `Interceptor` functions.
+ * It is particularly useful in scenarios where you want to apply one or more interceptors to different properties of your state.
+ *
+ * @template StateType This is the state type whose keys are used to associate interceptors.
+ * @property {Interceptor<StateType[Property]>[]} This optional property is an array of `Interceptor` functions associated with each `StateType` property.  
+ * Its key is derived using `keyof StateType` which provides the set of keys of an object.
  */
 export type Interceptors<StateType> = {
   [Property in keyof StateType]?: Interceptor<StateType[Property]>[];
